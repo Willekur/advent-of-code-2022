4e423edc0434295d4b7324741404502df51a982a
Sander Roosingh <sroosingh@transip.nl>
2022-12-02 11:28:50 +0100

    Day 1

---
+++ 2022_12_01/one.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+INPUT_FILE = "example_input_one"
+
+
+def main():
+    lines = get_input()
+    highest_calories = 0
+    current_calories = 0
+    for calories in lines:
+        if calories == "":
+            if highest_calories < current_calories:
+                highest_calories = current_calories
+                current_calories = 0
+        else:
+            current_calories = current_calories + int(calories)
+    print(highest_calories)
+
+
+def get_input():
+    with open(INPUT_FILE) as f:
+        lines_raw = f.readlines()
+
+    lines = []
+    for line in lines_raw:
+        lines.append(line.strip('\n'))
+
+    return lines
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2022_12_01/two.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+
+INPUT_FILE = "input_one"
+
+
+def main():
+    lines = get_input()
+    highest_calories = [0, 0, 0]
+    current_calories = 0
+    for calories in lines:
+        if calories == "":
+            check_calories(highest_calories, current_calories)
+            current_calories = 0
+        else:
+            current_calories = current_calories + int(calories)
+    check_calories(highest_calories, current_calories)
+    print(highest_calories[0] + highest_calories[1] + highest_calories[2])
+
+
+def check_calories(highest_calories, current_calories):
+    if highest_calories[0] < current_calories:
+        highest_calories[2] = highest_calories[1]
+        highest_calories[1] = highest_calories[0]
+        highest_calories[0] = current_calories
+    elif highest_calories[1] < current_calories:
+        highest_calories[2] = highest_calories[1]
+        highest_calories[1] = current_calories
+    elif highest_calories[2] < current_calories:
+        highest_calories[2] = current_calories
+
+
+def get_input():
+    with open(INPUT_FILE) as f:
+        lines_raw = f.readlines()
+
+    lines = []
+    for line in lines_raw:
+        lines.append(line.strip('\n'))
+
+    return lines
+
+
+if __name__ == "__main__":
+    main()
